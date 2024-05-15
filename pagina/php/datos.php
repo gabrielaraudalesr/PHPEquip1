208be8ecb40d420b3f771b8855428c3b60885f3f
manelNunez <manel.nunezfer@gmail.com>
2024-05-15 12:51:25 +0200

    Si

--- pagina/php/datos.php
+++ pagina/php/datos.php
@@ -1,19 +1,9 @@
 <?php
 
-<<<<<<< HEAD
 $host = "localhost";
 $usuario = "root";
 $pass="";
 $bd = "simarropop";
 $port = 3306;
-
-
-?>
-
-
-=======
 $nombre = "Manel";
-
-
 ?>
->>>>>>> 3dfff94c3207a424d492e75f09c55e4ae5874686
