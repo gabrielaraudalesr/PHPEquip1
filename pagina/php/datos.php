d9562994b6a235a49966cf169fd5842c490961c6
enriquenavarromartinez <enrique17anna@gmail.com>
2024-05-15 12:56:30 +0200

    manel

--- pagina/php/datos.php
+++ pagina/php/datos.php
@@ -3,7 +3,24 @@
 $host = "localhost";
 $usuario = "root";
 $pass="";
-$bd = "simarropop";
+$bd = "proyectoaplicacioncliente";
 $port = 3306;
-$nombre = "Manel";
+
+$conexion = mysqli_connect($host,$usuario,$pass,$bd,$port);
+
+$consulta="SELECT * FROM usuarios";
+$resultado=mysqli_query($conexion,$consulta);
+
+print "<table border>";
+while($fila=mysqli_fetch_array($resultado)){
+    $id=$fila["id"];
+    $nombre=$fila["nombre"];
+    $precio=$fila["precio"];
+    $descripcion=$fila["descripcion"];
+    
+    print "<tr><td>" . $id . "</td><td>" . $nombre . "</td><td>" . $precio . "</td><td>" . $descripcion . "</td></tr>"; 
+    
+}
+print "</table>";
+
 ?>
