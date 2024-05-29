0cff5f2510a90d7c359b04d32ead0a2da1a97ff7
El vostre nom <usuari@domini.com>
2024-05-29 08:57:17 +0200

    Display Imagen Perfil

--- pagina/php/funciones.php
+++ pagina/php/funciones.php
@@ -26,13 +26,14 @@ function comprobarLogin($user, $contrasena){
             }
         }
     } else {
-        $consulta="SELECT Usuario, Contrasena FROM usuarios";
+        $consulta="SELECT nombreUsuario, Contrasena FROM usuarios";
         $resultado=mysqli_query($conexion,$consulta);
 
         while ($fila=mysqli_fetch_array($resultado)) {
-            if ($user == $fila['Usuario']) {
+            if ($user == $fila['nombreUsuario']) {
                 if (password_verify($contrasena, $fila['Contrasena'])) {
                 header("Location: ../principal.html");
+
                 } else {
                     session_start();
                     $_SESSION['user_temp'] = isset($_POST['user']) ? $_POST['user'] : '';
@@ -58,14 +59,15 @@ function listarUsuarios(){
     
     $conexion=conectarBD();
     
-    $consulta="SELECT `IDUsuario`,`Nombre`,`Apellido`,`Contrasena`,`Poblacion`,DATE_FORMAT(`FechaNacimiento`, '%d-%m-%Y') 'FechaNacimiento',`Correo`,`ImagenPerfil` FROM usuarios";
+    $consulta="SELECT `IDUsuario`,`nombreUsuario`,`Nombre`,`Apellido`,`Contrasena`,`Poblacion`,DATE_FORMAT(`FechaNacimiento`, '%d-%m-%Y') 'FechaNacimiento',`Correo`,`ImagenPerfil` FROM usuarios";
     $resultado=mysqli_query($conexion,$consulta);
     
     
-        $lista="<table border=1 style='border-collapse: collapse;'><thead><tr><th>ID Usuario</th><th>Nombre</th><th>Apellido</th><th>Contraseña</th><th>Poblacion</th><th>Fecha Nacimiento</th><th>Correo</th></tr></thead><tbody>";
+        $lista="<table border=1 style='border-collapse: collapse;'><thead><tr><th>ID Usuario</th><th>NombreUsuario</th><th>Nombre</th><th>Apellido</th><th>Contraseña</th><th>Poblacion</th><th>Fecha Nacimiento</th><th>Correo</th></tr></thead><tbody>";
         while($fila=mysqli_fetch_array($resultado)){            
 
             $id=$fila["IDUsuario"];
+            $user=$fila["nombreUsuario"];
             $nombre=$fila["Nombre"];
             $apellido=$fila["Apellido"];
             $contrasena=$fila["Contrasena"];
@@ -73,11 +75,12 @@ function listarUsuarios(){
             $fechaNacimiento=$fila["FechaNacimiento"];
             $correo=$fila["Correo"];
             $imagenPerfil=$fila["ImagenPerfil"];
+            $imagenPerfil='<img src="data:image/jpeg;base64,'.base64_encode($imagenPerfil) .'" style="width: 15%;height:15%;"/>';
             
-        $lista .= "<tr><td>" . $id . "</td><td>" . $nombre . "</td><td>" . $apellido . "</td><td>" . $contrasena . "</td><td>" . $poblacion . "</td><td>" . $fechaNacimiento . "</td><td>" . $correo . "</td><td>" . $imagenPerfil . "</td></tr></tbody>"; 
+        $lista .= "<tr><td>" . $id . "</td><td>" . $user . "</td><td>" . $nombre . "</td><td>" . $apellido . "</td><td>" . $contrasena . "</td><td>" . $poblacion . "</td><td>" . $fechaNacimiento . "</td><td>" . $correo . "</td><td>" . $imagenPerfil . "</td></tr>"; 
             
         }
-        $lista.="</table>";
+        $lista.="</tbody></table>";
         
         return $lista;
         
